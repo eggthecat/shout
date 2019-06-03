5a68a372b03f9724951b3c8d98b1e0271bde2e27
Student <example@example.com>
2019-06-03 16:18:40 -0700

    create Calculator, BMI calculator, and jQuery stuff

--- scripts.js
+++ scripts.js
@@ -14,3 +14,21 @@ var number1 =parseInt(prompt("number1:"));
 var number2 = parseInt(prompt("number2:"));
 
 alert(add(number1, number2));
+
+
+var height = function(height) {
+  return height * height;
+};
+var weight = function(weight) {
+  return weight;
+};
+
+var multiply = function(num1, num2) {
+  return num1*num2;
+}
+
+
+var height = parseInt(prompt("height:"));
+var weight = parseInt(prompt("weight:"));
+
+alert((multiply(height, height) /weight));
