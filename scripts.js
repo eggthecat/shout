533064b557bb774816f2086aa41f0b52f2fd4293
Student <example@example.com>
2019-06-03 14:11:42 -0700

    create calculator with README, js and HTML

---
+++ scripts.js
@@ -0,0 +1,16 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+alert(add(10,5));
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 =parseInt(prompt("number1:"));
+var number2 = parseInt(prompt("number2:"));
+
+alert(add(number1, number2));
